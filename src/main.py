efc79dd9d2a2cc067bfc86e090c7f2f2b77d2045
genghk79 <genghk@gmail.com>
2026-01-01 18:15:51 +0800

    feature: add inquirer menu system

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from backtrack import Backtrack
 import general_utils
 import logging
 import sys
+import os
+import inquirer
 
 logger = logging.getLogger(__name__)
 general_utils.setup_logging()
@@ -20,39 +22,45 @@ print("=" * 60)
 print("Welcome to the Sudoku solver!")
 print("=" * 60)
 print()
-print("Would you like to:")
-print("1. Load a board/puzzle from a txt file, or")
-print("2. Reuse the previous board/puzzle, or")
-print("3. Enter a new board/puzzle?")
-choice = 0
-while not choice in {1,2,3}:
-    choice = input("Please choose option 1,2 or 3: ")
-    try:
-        choice = int(choice)
-    except:
-        pass
-print()
+
+questions = [
+    inquirer.List(
+        "choice",
+        message="Your latest board can be found in the savefile 'last_used_puzzle.txt'.\n\tWould you like to",
+        choices=[
+            ("Load a board/puzzle from a txt file in the 'savefiles' directory", 1),
+            ("Enter a new board/puzzle", 2),
+        ],
+    ),
+]
+answer = inquirer.prompt(questions)
+choice = answer["choice"]  # Returns 1, 2, or 3
 
 board = SudokuBoard()
-
 if choice == 1:
-    filename = input("Please enter the full name of the file to load: ")
-    try:
-        board.load_puzzle(filename)        
-    except Exception:
-        logger.exception(f"Failed to load file {filename}.")
+    savefiles_dir = "savefiles"
+    files = sorted(
+        [f for f in os.listdir(savefiles_dir) if f.endswith(".txt")],
+        key=lambda f: os.path.getmtime(os.path.join(savefiles_dir, f)),
+        reverse=True
+    )
+
+    if not files:
+        print("No saved puzzles found.")
         sys.exit(1)
+
+    questions = [
+        inquirer.List(
+            "filename",
+            message="Select a puzzle to load",
+            choices=files,
+        ),
+    ]
+    answer = inquirer.prompt(questions)
+    filename = os.path.join(savefiles_dir, answer["filename"])
+    board.load_puzzle(filename)    
     
-if choice == 2:
-    print("Load the previous board/puzzle")
-    try:
-        filename = 'savefiles/last_used_puzzle.txt'
-        board.load_puzzle(filename)
-    except Exception:
-        logger.exception(f"Failed to load file {filename}.")
-        sys.exit(1)
-    
-if choice == 3:
+else:
     print("Please enter the puzzle to solve row-by-row as comma separated rows of 9 numbers each.")
     print("You may use the following as a template:")
     print("0, 0, 0, 0, 0, 0, 0, 0, 0")
@@ -79,9 +87,16 @@ if choice == 3:
             print()
     board.set_puzzle(board_input)
     logger.info("New puzzle successfully entered from CLI.")
+    print()
 
-if choice in {1,3}:
-    board.save_puzzle('savefiles/last_used_puzzle.txt')
+    questions = [inquirer.Text("savename", message="Please provide a file name to save this puzzle to")]
+    answer = inquirer.prompt(questions)
+    savename = answer['savename']
+    if savename.split('.')[-1] != 'txt':
+        savename = savename + '.txt'
+    board.save_puzzle('savefiles/'+savename)
+
+board.save_puzzle('savefiles/last_used_puzzle.txt')
 
 print('-' * 20)
 print("Puzzle to solve")
@@ -92,27 +107,27 @@ print()
 print("-" * 60)
 print()
 
-print("We shall proceed to solve the puzzle. Would you like to use: ")
-print("1. strategies as taught on sudoku.com, or")
-print("2. brute force i.e. backtracking algorithm?")
-solve_by = 0
-while not solve_by in {1,2}:
-    solve_by = input("Please choose option 1 or 2 (or 'q' to quit): ")
-    try:
-        if solve_by == 'q':
-            raise UserQuit
-        solve_by = int(solve_by)
-    except UserQuit:
-        print("quitting...")
-        sys.exit(0)
-    except:
-        pass
+questions = [
+        inquirer.List(
+            "solve_by",
+            message="We can now proceed to solve the puzzle. How would you like to do it?",
+            choices=[("Using strategies as taught on sudoku.com", 1),
+                     ("By brute force i.e. backtracking algorithm", 2),
+                     ("I don't want to solve the puzzle, let's quit", 3)],
+        ),
+    ]
+answer = inquirer.prompt(questions)
+solve_by = answer['solve_by']
+
 if solve_by == 1:
     strat = SudokuStrats(board)
     strat.solve()
-else:
+elif solve_by == 2:
     bt = Backtrack(board)
     bt.solve()
+else:
+    print("quitting...")
+    sys.exit(0)
 
 print("-" * 60)
 print()
